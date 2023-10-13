7c20828a346ab901b4470cd13815580be5c70155
Shannon Schrauwen <shannon.schrauwen@gmail.com>
2023-10-13 17:26:14 +0300

    Portfolio page setup

---
+++ portfolio-website-ts/src/App.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import './styles/main.scss';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+      </header>
+
+      <main></main>
+
+      <footer></footer>
+    </div>
+  );
+}
+
+export default App;
